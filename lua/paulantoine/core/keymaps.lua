357615ecd8589d6b57391f2eaf37ab3048061898
Paul-Antoine Leveilley <paul-antoine.leveilley@dauphine.eu>
2025-06-04 23:30:12 +0200

    fix typo

--- lua/paulantoine/core/keymaps.lua
+++ lua/paulantoine/core/keymaps.lua
@@ -4,6 +4,6 @@ local keymap = vim.keymap --for conciseness
 
 keymap.set("i", "<leader>jk", "<ESC>", {desc = "Exit insert mode with jk"})
 
-keymap.set("n","<leader>nh", ":nolh<CR>", {desc= "clear search history highlights"})
+keymap.set("n","<leader>nh", ":nohl<CR>", {desc= "clear search history highlights"})
 
 
