bc65b564a079858f2b7c0e7a6d5b6990b56a7fff
Paul-Antoine Leveilley <paul-antoine.leveilley@stackease.ai>
2025-06-09 19:24:31 +0200

    [func] surround plugin + mason (lsp)

--- lua/paulantoine/lazy.lua
+++ lua/paulantoine/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("paulantoine.plugins" , {
+require("lazy").setup({ { import = "paulantoine.plugins" }, { import = "paulantoine.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
