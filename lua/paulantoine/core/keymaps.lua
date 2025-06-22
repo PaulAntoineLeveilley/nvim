6bec111ee903a811240390097645814835e723cb
Paul-Antoine Leveilley <paul-antoine.leveilley@dauphine.eu>
2025-06-22 20:14:47 +0200

    [func] remap $ to é and ^ to ç

--- lua/paulantoine/core/keymaps.lua
+++ lua/paulantoine/core/keymaps.lua
@@ -18,3 +18,6 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
+-- remap vim keys
+vim.keymap.set({ "n", "v" }, "é", "$", { noremap = true })
+vim.keymap.set({ "n", "v" }, "ç", "^", { noremap = true })
