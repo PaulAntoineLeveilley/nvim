5f63c421bf4d0654c830591e8408fad8b618fe40
Paul-Antoine Leveilley <paul-antoine.leveilley@stackease.ai>
2025-07-08 10:31:52 +0200

    [func]relative numbers for outline

--- init.lua
+++ init.lua
@@ -5,3 +5,13 @@ vim.opt.fillchars = {
   foldopen = "", -- flèche vers le bas : bloc déplié
   foldclose = "", -- flèche vers la droite : bloc plié
 }
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "Outline",
+  callback = function()
+    vim.defer_fn(function()
+      if vim.bo.filetype == "Outline" then
+        vim.opt_local.relativenumber = true
+      end
+    end, 0)
+  end,
+})
